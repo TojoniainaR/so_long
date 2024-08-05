08897d8ace599acd0e69d87dae4cc0f47df397fa
Tojoniaina Fenosoa Gian Elias Randriamanana <torandri@c1r8s1.42antananarivo.mg>
2024-08-05 17:59:05 +0300

    main

--- main.c
+++ main.c
@@ -6,32 +6,11 @@
 /*   By: torandri <torandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 08:23:36 by torandri          #+#    #+#             */
-/*   Updated: 2024/07/22 16:09:24 by torandri         ###   ########.fr       */
+/*   Updated: 2024/08/05 13:06:02 by torandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-int	ft_error(t_data data)
-{
-	if (data.x <= 3 && data.y <= 3)
-	{
-		ft_printf("Error\nMap too little\n");
-		free(data.file);
-		return (1);
-	}
-	if (ft_check_map_error(data.x, data.y, data.file) == 1)
-	{
-		free(data.file);
-		return (1);
-	}
-	if (ft_check_map_valid(&data) == 1)
-	{
-		free(data.file);
-		return (1);
-	}
-	return (0);
-}
 
 int	main(int argc, char *argv[])
 {
@@ -48,6 +27,7 @@ int	main(int argc, char *argv[])
 	data.vars.mlx_win = mlx_new_window(data.vars.mlx, \
 	data.x * SCALE, data.y * SCALE, "so_long");
 	ft_get_pos_player(&data);
+	ft_get_pos_exit(&data);
 	ft_put_image(&data);
 	free(data.file);
 	mlx_key_hook(data.vars.mlx_win, ft_key_events, &data);
